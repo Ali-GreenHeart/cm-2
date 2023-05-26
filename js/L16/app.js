55b96fa6046c09226f264d34a3be88dab0acc041
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-05-26 09:29:10 +0400

    add try/catch

---
+++ js/L16/app.js
@@ -0,0 +1,16 @@
+// const ad = prompt('adinizi daxil edin: ')
+
+// // syntax error
+// // runtime errorlari!
+// let test = 12
+// try {
+//     console.log(test)
+// } catch (err) {
+//     console.log('error cixdi! ad null ola bilmez!')
+//     console.log(err.message)
+//     console.log(err.stack)
+// }
+
+// console.log('heyat davam edir')
+
+// // exception (error) handling
