68e34b1381874283fdfede73d8c952886b2ed1df
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-05-26 10:16:42 +0400

    add json

--- js/L16/app.js
+++ js/L16/app.js
@@ -1,22 +1,54 @@
-// const ad = prompt('adinizi daxil edin: ')
+// // const ad = prompt('adinizi daxil edin: ')
 
-// // syntax error
-// // runtime errorlari!
-// let test = 12
-try {
-    console.log(test)
-} catch (err) {
-    console.log('error cixdi! ad null ola bilmez!')
-    console.log(err.message)
-    console.log(err.stack)
-} finally {
-    console.log('sonda .... ')
-}
-// pollute
-console.log('heyat davam edir')
+// // // syntax error
+// // // runtime errorlari!
+// // let test = 12
+// try {
+//     console.log(test)
+// } catch (err) {
+//     console.log('error cixdi! ad null ola bilmez!')
+//     console.log(err.message)
+//     console.log(err.stack)
+// } finally {
+//     console.log('sonda .... ')
+// }
+// // pollute
+// console.log('heyat davam edir')
 
-// // exception (error) handling
+// // // exception (error) handling
 
+
+
+
+
+// 1. configuration files 
+// 2. data alis-verisi
+
+// number, boolean, string, null, [], {}
+
+
+// const obj = {
+//     name: 'test',
+//     gender: true,
+//     age: 12,
+//     friends: [
+//         "ali",
+//         "aydan"
+//     ],
+//     car: null,
+//     enemies: undefined,
+//     doing: () => {
+//         console.log('coding...')
+//     }
+// }
+
+// const dataFromFe = JSON.stringify(obj)
+// console.log(dataFromFe)
+
+// // BackendSide
+
+// const dataInBe = JSON.parse(dataFromFe)
+// console.log(dataInBe)
 
 
 
