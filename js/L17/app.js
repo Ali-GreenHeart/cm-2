5c89e81fef8203aba26fb5ba84f3a828be4a1ee9
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-05-29 10:19:14 +0400

    add promise

---
+++ js/L17/app.js
@@ -0,0 +1,50 @@
+// // synchronous
+
+// // A  -> backend-en data getirir (5san-10san)
+// // B -> header-in kodlari 
+// // C -> side menu
+
+// // asynchronous
+
+
+
+
+// // setTimeout(() => {
+// //     console.log('salam')
+// // }, 0);
+// // console.log('salam2')
+
+
+
+
+// axios.get(url).then(({ data }) => {
+//     console.log(data)
+// })
+
+// // console.log('heyyyy')
+
+// // boilerplate  -> 
+
+
+
+
+// Promise -> soz vermek
+
+// 3cu gun saat 9da dersimiz olacaq
+// 1. pending
+// 2. fulfilled
+// 3. rejected
+
+
+// const url = `https://jsonplaceholder.typicode.com/users`
+
+// fetch(url)
+//     .then((resp) => resp.json())
+//     .then((users) => {
+//         users.forEach((user) => {
+//             document.write(
+// `<p>${user.id} Name: ${user.name} email:${user.email} Company:${user.company.name}</p>`)
+//         });
+//     })
+
+
